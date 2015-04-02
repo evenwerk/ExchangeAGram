229aeaeb479d09e8b0d66ba2541829d93b5c32eb
evenwerk <evenwerk@gmail.com>
2015-04-02 18:14:14 +0200

    Delete Bridging-Header.h

--- ExchangeAGram/Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Bridging-Header.h
-//  ExchangeAGram
-//
-//  Created by Tim Even on 31-03-15.
-//  Copyright (c) 2015 evenwerk. All rights reserved.
-//
-
-#<import <Supporting Files/FacebookSDK.framework/Headers/FacebookSDK.h>
