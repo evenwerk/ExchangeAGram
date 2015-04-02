193b09504b855a29f8fb32e7b3dcc1cbda80a6c1
evenwerk <evenwerk@gmail.com>
2015-04-02 11:35:49 +0200

    third rev
    error

--- ExchangeAGram/Bridging-Header.h
+++ ExchangeAGram/Bridging-Header.h
@@ -7,5 +7,3 @@
 //
 
 #<import <FacebookSDK/FacebookSDK.h>
-
-#endif
--- ExchangeAGram/ProfileViewController.swift
+++ ExchangeAGram/ProfileViewController.swift
@@ -8,91 +8,47 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController, FBSDKLoginButtonDelegate {
+class ProfileViewController: UIViewController, FBLoginViewDelegate {
     
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var fbLoginView: FBSDKLoginButton!
+    @IBOutlet weak var fbLoginView: FBLoginView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.fbLoginView.delegate = self
-        self.fbLoginView.readPermissions = ["public_profile"]
-        self.fbLoginView.publishPermissions = ["publish_actions"]
-        
-        NSNotificationCenter.defaultCenter().addObserver(
-            self,
-            selector: "fbProfileChanged:",
-            name: FBSDKProfileDidChangeNotification,
-            object: nil)
-        FBSDKProfile.enableUpdatesOnAccessTokenChange(true)
-        
-        // If we have a current Facebook access token, force the profile change handler
-        if ((FBSDKAccessToken.currentAccessToken()) != nil)
-        {
-            self.fbProfileChanged(self)
+        self.fbLoginView.readPermissions = ["public_profile", "publish_actions"]
         }
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        if (error != nil) {
-            println( "\(error.localizedDescription)" )
-        }
-        else if (result.isCancelled) {
-            // Logged out?
-            println( "Login Cancelled")
-        }
-        else {
-            // Logged in?
-            println( "Logged in")
-        }
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+        profileImageView.hidden = false
+        nameLabel.hidden = false
     }
     
-    func fbProfileChanged(sender: AnyObject!) {
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        println(user)
         
-        let fbProfile = FBSDKProfile.currentProfile()
-        if (fbProfile != nil)
-        {
-            // Fetch & format the profile picture
-            let strProfilePicURL = fbProfile.imagePathForPictureMode(FBSDKProfilePictureMode.Square, size: self.profileImageView.frame.size)
-            let url = NSURL(string: strProfilePicURL, relativeToURL: NSURL(string: "http://graph.facebook.com/"))
-            let imageData = NSData(contentsOfURL: url!)
-            let image = UIImage(data: imageData!)
-            
-            self.nameLabel.text = fbProfile.name
-            self.profileImageView.image = image
-            
-            self.lblName.hidden = false
-            self.imgVwProfile.hidden = false
-        }
-        else
-        {
-            self.nameLabel.text = ""
-            self.profileImageView.image = UIImage(named: "")
-            
-            self.lblName.hidden = true
-            self.imgVwProfile.hidden = true
-        }
+        nameLabel.text = user.name
+        
+        let userImageURL = "https://graph.facebook.com/\(user.objectID)/picture?type=small"
+        let url = NSURL(string: userImageURL)
+        let imageData = NSData(contentsOfURL: url!)
+        let image = UIImage(data: imageData!)
+        profileImageView.image = image
     }
     
-    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-        
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        profileImageView.hidden = true
+        nameLabel.hidden = true
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func loginView(loginView: FBLoginView!, handleError error: NSError!) {
+        println("Error: \(error.localizedDescription)")
     }
-    */
-
 }
